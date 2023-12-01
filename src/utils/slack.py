17ce48c2d694691d1f4281b693014eb0f66110ad
Christoph Becker <mail@ch-becker.de>
2023-12-01 12:11:29 +0100

    Add slack sender

---
+++ src/utils/slack.py
@@ -0,0 +1,26 @@
+import requests
+from decouple import config
+from loguru import logger
+
+WEBHOOK_URL = config('WEBHOOK_URL', cast=str, default="")
+
+
+def trigger_slack(msg: str) -> None:
+    if not WEBHOOK_URL:
+        logger.warning('no WEBHOOK_URL configured')
+        logger.info(msg)
+        return
+    jsons = {
+        "text": msg
+    }
+    try:
+        logger.debug(f"To Slack: {msg}")
+        ret = requests.post(WEBHOOK_URL, json=jsons)
+        logger.debug(ret.json())
+    except requests.exceptions.SSLError as ex:
+        logger.error('SSL Error, could not send slack msg')
+        logger.error(ex)
+    except Exception as ex:
+        logger.error('Other exception, could not send slack msg')
+        logger.error(ex)
+    return
